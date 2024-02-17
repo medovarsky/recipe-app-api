3e83d5b3426bb8909fef9c64a3423c17150f6386
Lubos <lubos@medovarsky.com>
2024-02-17 21:23:55 +0100

    another set of fixes against the original files

--- app/core/tests/test_admin.py
+++ app/core/tests/test_admin.py
@@ -31,3 +31,10 @@ class AdminSiteTests(TestCase):
 
         self.assertContains(res, self.user.name)
         self.assertContains(res, self.user.email)
+
+    def test_create_user_page(self):
+        """Test the create user page works. """
+        url = reverse("admin:core_user_add")
+        res = self.client.get(url)
+
+        self.assertEqual(res.status_code, 200)

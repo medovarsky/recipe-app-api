e6f2bc1acf84c307c2338bfb9870d79b9fb31e66
Lubos <lubos@medovarsky.com>
2024-02-18 19:34:51 +0100

    make flake8 happy

--- app/core/tests/test_user_api.py
+++ app/core/tests/test_user_api.py
@@ -13,6 +13,7 @@ CREATE_USER_URL = reverse('user:create')
 TOKEN_URL = reverse('user:token')
 ME_URL = reverse('user:me')
 
+
 def create_user(**params):
     """Create and return a new user."""
     return get_user_model().objects.create_user(**params)
@@ -106,6 +107,7 @@ class PublicUserApiTests(TestCase):
         res = self.client.get(ME_URL)
 
         self.assertEqual(res.status_code, status.HTTP_401_UNAUTHORIZED)
+
 
 class PrivateUserApiTests(TestCase):
     """Test API requests that require authentication."""
